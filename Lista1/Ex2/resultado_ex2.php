566bb1c46c38aad991a65b6ceef6e2e0143ac429
guilherm-hsbe <99516132+guilherm-hsbe@users.noreply.github.com>
2022-03-04 22:19:47 -0300

    Aula 04/03

--- Lista1/Ex2/resultado_ex2.php
+++ Lista1/Ex2/resultado_ex2.php
@@ -6,23 +6,19 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Cálculo Exercício 1</title>
+    <title>Cálculo Exercício 2</title>
   </head>
+
   <body class="container">
-    <h1>Cálculo Exercício 1</h1>
+    <h1>Cálculo Exercício 2</h1>
 
     <?php
-        $valorpago = $_POST['valorpago']; 
-        $valorpreco = $_POST['valorpreco'];
-        echo "O valor pago é: $valorpago <br/> 
-          O preço do produto é: $valorpreco <br/>";
-
-        $troco = $valorpago - $valorpreco;
-        echo "<br/>O valor do troco é: $troco";
-
+       $valorquilo = $_POST['valorquilo']; 
+       $quantquilo = $_POST['quantquilo'];
+       echo "O valor de vina é: $valorquilo <br/> 
+         A quantidade de vina é: $quantquilo <br/>";
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
   </body>
 </html>
